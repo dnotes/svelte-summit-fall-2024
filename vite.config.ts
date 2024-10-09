149580a7b672b65a473db3f60c2ae2276c8ca702
David Hunt <git@dnotes.net>
2024-10-10 12:06:07 +1300

    install and configure quickpickle and @quickpickle/playwright

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { quickpickle } from 'quickpickle'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(),quickpickle()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}','src/**/*.feature'],
+		setupFiles: ['./tests/gherkin.steps.ts'],
 	}
 });
